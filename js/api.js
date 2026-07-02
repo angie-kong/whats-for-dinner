9a3bdf4f3cfc39bb3aaa23ab0034cb8de52370c5
Angela Kong <angela.kong@outlook.com>
2026-07-02 15:38:57 -0400

    Add getRandomMeal function, confirmed working

---
+++ js/api.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+
+async function getRandomMeal() {
+    try {
+        const response = await fetch(`${BASE_URL}/random.php`);
+        const data = await response.json();
+        return data.meals[0];
+    } catch (error) {
+        console.error("Error fetching random meal:", error);
+    }
+}
+
+
+
+
+getRandomMeal().then(meal => console.log(meal));
